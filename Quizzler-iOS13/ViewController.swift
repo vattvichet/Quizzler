fdc34e1bd9c20766c57dd3b30eb85c432d49c5a3
vattvichet <vichetvattedu@gmail.com>
2022-04-08 23:19:25 +0700

    Loop the quesions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     
     let quiz = [
         ["4+2 = 6", "True"],
-        ["5-3 > 1", "Ture"],
-       [ "3+8 < 10", "Flase"]
+        ["5-3 > 1", "True"],
+       [ "3+8 < 10", "False"]
     ]
     
     var questionNumber = 0
@@ -36,8 +36,16 @@ class ViewController: UIViewController {
         } else {
             print("Wrong!")
         }
-        
+        if questionNumber + 1 < quiz.count {  //.count returens the number of values stored in an array
+            
+            //question + 1 ,, it starts from 0,1,2 when it is 2 + 1 = 3 is false with < quiz.count, so it stops increasing
+            
         questionNumber += 1 //add one and one
+            
+        } else {
+            questionNumber = 0 // change back 1st question
+        }
+        
         updateUI()
     }
     
@@ -45,11 +53,7 @@ class ViewController: UIViewController {
         questionLabel.text = quiz[questionNumber][0]// (2D array),,question number [0] is for 4+2=6
     }
     
-    
-    
-    
-    
-    
+
     
 }
 
