739024c1f604fbf210696aaae7aa5ea32da70694
vattvichet <vichetvattedu@gmail.com>
2022-04-20 22:08:54 +0700

    Data Structure

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     
     let quiz = [
-        ["4+2 = 6", "True"],
-        ["5-3 > 1", "True"],
-       [ "3+8 < 10", "False"]
+        Question(q: "A slug's blood is green.", a: "True"),
+                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+                Question(q: "Google was originally called 'Backrub'.", a: "True"),
+                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+
     ]
     
     var questionNumber = 0
@@ -29,12 +39,15 @@ class ViewController: UIViewController {
     @IBAction func answerbutton(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle// True or False
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
+        // Or
+        //let actualQuestion = quiz[questionNumber]
+        //let actualAnswer = actualQuestion.answer
         
         if userAnswer == actualAnswer {
-            print("Right!")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Wrong!")
+            sender.backgroundColor = UIColor.red
         }
         if questionNumber + 1 < quiz.count {  //.count returens the number of values stored in an array
             
@@ -46,11 +59,15 @@ class ViewController: UIViewController {
             questionNumber = 0 // change back 1st question
         }
         
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
     }
     
-    func updateUI() { //updateUI function used for update the question after the button got pressed.
-        questionLabel.text = quiz[questionNumber][0]// (2D array),,question number [0] is for 4+2=6
+    @objc func updateUI() { //updateUI function used for update the question after the button got pressed.
+        questionLabel.text = quiz[questionNumber].text
+        
+        trueButton.backgroundColor = UIColor.clear
+        fasleButton.backgroundColor = UIColor.clear
     }
     
 
