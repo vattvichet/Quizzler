3ba77f4f77a60cddf840d935d59cbec2ab66f1be
vattvichet <vichetvattedu@gmail.com>
2022-04-24 22:01:01 +0700

    MVC patern 2

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -24,35 +24,27 @@ class ViewController: UIViewController {
     @IBAction func answerbutton(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle! // True or False
-        quizBrain.checkAnswer(userAnswer)
-        // Or
-        //let actualQuestion = quiz[questionNumber]
-        //let actualAnswer = actualQuestion.answer
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+      
         
-        if userAnswer == actualAnswer {
+        if userGotItRight == actualAnswer {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
         }
-        if questionNumber + 1 < quiz.count {  //.count returens the number of values stored in an array
-            
-            //question + 1 ,, it starts from 0,1,2 when it is 2 + 1 = 3 is false with < quiz.count, so it stops increasing
-            
-        questionNumber += 1 //add one and one
-            
-        } else {
-            questionNumber = 0 // change back 1st question
-        }
+        
+        quizBrain.nextQuestion()
         
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
     
     @objc func updateUI() { //updateUI function used for update the question after the button got pressed.
-        questionLabel.text = quiz[questionNumber].text
+        questionLabel.text = quizBrain.getQuestionText()
         trueButton.backgroundColor = UIColor.clear
         fasleButton.backgroundColor = UIColor.clear
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        
+        progressBar.progress = quizBrain.getProgress()
     }
     
 
